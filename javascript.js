da5885af15fa72e7aad1bb1b2773ebdaab86b7cb
Sandip Jaiswar <jaiswarsandeep00@gmail.com>
2023-10-16 19:26:38 +0530

    updated

--- javascript.js
+++ javascript.js
@@ -1,1 +1,27 @@
-document.write("This is external javascript file")
+// let x=document.getElementById("head");
+// console.log(x);
+// x.innerHTML="pritam"
+// x.style.fontVariant="small-caps"
+// x.style.color='brown'
+// x.innerHTML ='New DOM'
+
+// let a=document.getElementsByClassName('java');
+// a[0].style.color='blue'
+// a[2].innerHTML="kuch bhi"
+
+// var b=document.getElementsByTagName('li');
+// b[1].innerHTML='Aquaman'
+
+// let y=document.getElementsByClassName('java');
+// let y=document.getElementsByTagName('li')
+// console.log(y)
+//  for (var i = 0; i < y.length; i++) {
+//     y[i].style.color ='red';
+//  }
+
+var z=document.querySelectorAll('.java');
+console.log(z);
+    z[3].style.color ='blue';
+
+
+document.write("Hello");
